2c6dc07842b7d89ccf4aeb4b11d6733cfbeefb3f
MixaPVO <mikhail-mixa@mail.ru>
2026-09-12 11:01:09 +0700

    Исправлены комментарии к функциям

--- main.cpp
+++ main.cpp
@@ -6,12 +6,13 @@
 using namespace std;
 
 // ===== Расчётные функции варианта =====
-// Площадь прямоугольника со сторонами a и b
+// Функция перевода километров в мили
 double kmToMiles(double km)
 {
     return km * 0.621371;
 }
 
+//Функция перевода милей в километры
 double milesToKm(double mi)
 {
     return mi * 1.609344;
